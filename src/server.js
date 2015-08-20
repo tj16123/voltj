19e336ea6904cc22be17755e1b70d37d0b05f029
TJ Moore <tj16123@gmail.com>
2015-08-19 22:13:46 -0600

    First commit of initial setup

---
+++ src/server.js
@@ -0,0 +1,9 @@
+var express =  require('express');
+var app = express();
+app.use(express.static(__dirname + ''));
+
+app.get('/', function(req, res){
+  res.sendfile('/index.html');
+});
+
+app.listen(3000);
